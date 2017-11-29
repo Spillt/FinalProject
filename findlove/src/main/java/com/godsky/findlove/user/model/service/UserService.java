08ecead9bf2875bdbe3cffb9e7ff16a81ccf229d
namcw <93x02x09>
2017-11-29 15:17:13 +0900

    reset

---
+++ findlove/src/main/java/com/godsky/findlove/user/model/service/UserService.java
@@ -0,0 +1,37 @@
+package com.godsky.findlove.user.model.service;
+
+import com.godsky.findlove.user.model.vo.User;
+
+public interface UserService {
+
+	User loginCheck();
+
+	boolean createUser();
+
+	int checkEmail();
+
+	int checkName();
+
+	int checkNickName();
+
+	int checkPwd();
+
+	String sendNewPwd();
+
+	String randomCreatePwd();
+
+	int modifyPwd();
+
+	int changeUserState();
+
+	int updateUserInfo();
+
+	int createUserProfile();
+
+	int createIdealProfile();
+
+	
+	
+	
+
+}
