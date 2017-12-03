dfb94eec7c28985b0f893ba2743554834f88344b
cho <naksu45@gmail.com>
2017-12-04 00:17:03 +0900

    NH
    main>eventboard

---
+++ findlove/src/main/java/com/godsky/findlove/main/eventboard/poll/model/service/PollService.java
@@ -0,0 +1,23 @@
+package com.godsky.findlove.main.eventboard.poll.model.service;
+
+import java.util.List;
+
+public class PollService {
+	
+	void insertPollInfo() {}
+
+	List deletePollInfo(){
+		return null;
+		}
+	
+	List selectPollDetail(){
+		return null;		
+	}
+	
+	List selectPollList(){
+		return null;		
+	}
+	
+	void updatePoll(){}
+
+}
