62617489cfde396f8a1f385c2de4c752bea5912a
chonamhun <naksu45@gmail.com>
2017-12-08 09:21:45 +0900

    service

--- findlove/src/main/java/com/godsky/findlove/main/matchboard/qna/service/MatchQnAServiceImpl.java
+++ findlove/src/main/java/com/godsky/findlove/main/matchboard/qna/service/MatchQnAServiceImpl.java
@@ -3,11 +3,12 @@ package com.godsky.findlove.main.matchboard.qna.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.godsky.findlove.main.matchboard.qna.dao.MatchQnADao;
 import com.godsky.findlove.main.matchboard.qna.vo.MatchQnA;
 import com.godsky.findlove.user.model.vo.User;
-
+@Service("MatchQnAService")
 public class MatchQnAServiceImpl implements MatchQnAService{
 	
 	@Autowired
--- findlove/src/main/java/com/godsky/findlove/main/matchboard/score/service/MatchScoreServiceImpl.java
+++ findlove/src/main/java/com/godsky/findlove/main/matchboard/score/service/MatchScoreServiceImpl.java
@@ -3,11 +3,12 @@ package com.godsky.findlove.main.matchboard.score.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.godsky.findlove.main.matchboard.score.dao.MatchScoreDao;
 import com.godsky.findlove.main.matchboard.score.vo.MatchScore;
 import com.godsky.findlove.user.model.vo.User;
-
+@Service("MatchScoreService")
 public class MatchScoreServiceImpl implements MatchScoreService{
 	@Autowired
 	MatchScoreDao matchScoreDao;
